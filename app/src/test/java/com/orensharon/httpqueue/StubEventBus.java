9f57e97fab499f76cdb0ab538924e10c27c9a6bf
atlantiumtech <sharon@atlantium.com>
2020-10-12 08:36:28 +0300

    visualization vm test

---
+++ app/src/test/java/com/orensharon/httpqueue/StubEventBus.java
@@ -0,0 +1,11 @@
+package com.orensharon.httpqueue;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class StubEventBus extends EventBus {
+
+    @Override
+    public void post(Object event) {
+
+    }
+}
--- app/src/test/java/com/orensharon/httpqueue/presentation/vm/VisualizationVMTest.java
+++ app/src/test/java/com/orensharon/httpqueue/presentation/vm/VisualizationVMTest.java
@@ -1,18 +1,46 @@
 package com.orensharon.httpqueue.presentation.vm;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import com.orensharon.httpqueue.StubEventBus;
+import com.orensharon.httpqueue.data.event.RequestStateChangedEvent;
+
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.hamcrest.core.Is.is;
+
 @RunWith(JUnit4.class)
 public class VisualizationVMTest {
 
- /*   private VisualizationVM viewModel;*/
+    //It will tell JUnit to force tests to be executed synchronously,
+    // especially when using Architecture Components.
+    @Rule
+    public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
+
+    private StubEventBus eventBus;
+
+    private VisualizationVM viewModel;
+
+    @Before
+    public void setUp() {
+        this.viewModel = new VisualizationVM(this.eventBus);
+    }
 
     @Test
     public void testNewSuccessRequestEvent() {
-        Assert.assertTrue(true);
+        long now = System.currentTimeMillis();
+        //this.viewModel.getLastSuccessEvent().observeForever(mock(Observer.class));
+        this.viewModel.onRequestStateChangedEvent(new RequestStateChangedEvent(1, now, true));
+        //Assert.assertThat(this.viewModel.getLastSuccessEvent().hasActiveObservers(), is(true));
+        Assert.assertThat(this.viewModel.getSuccessPercentage().getValue(), is(100));
+        Assert.assertNotNull(this.viewModel.getLastSuccessEvent().getValue());
+        Assert.assertEquals(now, this.viewModel.getLastSuccessEvent().getValue().ts);
+        Assert.assertEquals(1, this.viewModel.getAllSuccessEvents().size());
+        Assert.assertEquals(0, this.viewModel.getAllFailedEvents().size());
     }
 
     /*@Test
